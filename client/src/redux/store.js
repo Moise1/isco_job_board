777a206868d871c45ac6303c64f5c1cbb6b6c256
Moise1 <rwibutso.mo@gmail.com>
2025-08-12 22:35:42 +0200

    refactor job apply

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -7,6 +7,6 @@ export const store = configureStore({
   reducer: {
     users: usersReducer,
     jobs: jobsReducer,
-    jobApplication: jobApplicationReducer
+    jobApplications: jobApplicationReducer
   },
 });
