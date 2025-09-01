4b5e3dd18745f199f8e65a6bec4b4ea240cdd723
Mac <mac@MacBook-Pro.local>
2025-09-01 07:28:18 +0200

    edit allowed http verbs

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: "http://localhost:3000",   // React dev server
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
